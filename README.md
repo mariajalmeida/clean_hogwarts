fb9a9c75e79b00347661afc5c982538a6d93a806
mariajalmeida <mari9d91@stud.kea.dk>
2021-02-13 16:46:40 +0100

    table displayed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 // this is a clean up for the hogwarts data
 // here's the live preview:
+https://mariajalmeida.github.io/clean_hogwarts/
--- data.js
+++ data.js
@@ -9,45 +9,54 @@ function fetchData() {
         .then(transcribeData);
 }
 
+const allStudents = [];
+
 // here's all the students
 function transcribeData(students) {
-    students.forEach(eachStudent);
+    students.forEach(s => {
+        const Student = {
+            firstName: "",
+            middleName: "",
+            lastName: "",
+            nickname: "",
+            gender: "",
+            house: ""
+        }
+        const student = Object.create(Student);
+
+        // remove if any space around and then find the first available space after the first name
+        const removeSpace = s.fullname.trim();
+        const firstSpace = removeSpace.indexOf(" ");
+        const lastSpace = removeSpace.lastIndexOf(" ");
+
+        // remember "Leanne" is not correct yet
+        student.firstName = removeSpace.substring(0, firstSpace);
+        student.firstName = removeSpace.substring(0, 1).toUpperCase() + removeSpace.substring(1, firstSpace).toLowerCase();
+        // console.log(`First name _${student.firstName}_`)
+
+        // remember "ernie"'s quotes need to be removed
+        student.middleName = removeSpace.substring(firstSpace + 1, lastSpace);
+        if (student.middleName.length > 2) {
+            student.middleName = removeSpace.substring(firstSpace + 1, firstSpace + 2).toUpperCase() + removeSpace.substring(firstSpace + 2, lastSpace).toLowerCase();
+            // console.log(`Second name _${student.middleName}_`);
+        } else {
+            // console.log("denied");
+        }
+
+        student.lastName = removeSpace.substring(lastSpace);
+        student.lastName = removeSpace.substring(lastSpace + 1, lastSpace + 2).toUpperCase() + removeSpace.substring(lastSpace + 2);
+
+        const removeSpaceHouse = s.house.trim();
+        student.house = removeSpaceHouse;
+        student.house = removeSpaceHouse.substring(0, 1).toUpperCase() + removeSpaceHouse.substring(1).toLowerCase();
+        student.gender = s.gender;
+
+        allStudents.push(student);
+    });
+
+    cleanData();
 }
 
-// here's each student separately
-function eachStudent(s) {
-    console.log(s);
-    const Student = {
-        firstName: "",
-        middleName: "",
-        lastName: "",
-        nickname: ""
-    }
-    const studentName = Object.create(Student);
-
-    // remove if any space around and then find the first available space after the first name
-    const removeSpace = s.fullname.trim();
-    const firstSpace = removeSpace.indexOf(" ");
-    console.log(removeSpace, "real data")
-
-    const lastSpace = removeSpace.lastIndexOf(" ");
-
-    // remember "Leanne" is not correct yet
-    studentName.firstName = removeSpace.substring(0, firstSpace);
-    studentName.firstName = removeSpace.substring(0, 1).toUpperCase() + removeSpace.substring(1, firstSpace).toLowerCase();
-    // console.log(`First name _${studentName.firstName}_`)
-
-    // remember "ernie"'s quotes need to be removed
-    studentName.middleName = removeSpace.substring(firstSpace + 1, lastSpace);
-    if (studentName.middleName.length > 2) {
-        studentName.middleName = removeSpace.substring(firstSpace + 1, firstSpace + 2).toUpperCase() + removeSpace.substring(firstSpace + 2, lastSpace).toLowerCase();
-        // console.log(`Second name _${studentName.middleName}_`);
-    } else {
-        // console.log("denied");
-    }
-
-    studentName.lastName = removeSpace.substring(lastSpace);
-    studentName.lastName = removeSpace.substring(lastSpace + 1, lastSpace + 2).toUpperCase() + removeSpace.substring(lastSpace + 2);
-    console.log(studentName.lastName);
-
+function cleanData() {
+    console.table(allStudents);
 }
